80564f7b3c52e3fd2a93c7d7f8d26c9e5c14d70b
Ambesh Tiwari <30859788+ambessh@users.noreply.github.com>
2026-06-24 21:31:26 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,3 +10,4 @@ export const config = {
   matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)", "/(api|trpc)(.*)"],
 };
 console.log("middleware.ts");
+const product = "aicodereview";
